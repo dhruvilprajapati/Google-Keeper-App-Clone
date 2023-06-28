cde52d187cac083b4f8d123f5e05b842d41eded5
Dhruvil Prajapati <prajapatidhruvil.a@gmail.com>
2023-06-28 17:06:17 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                <HighlightIcon />
+                Keeper App</h1>
+        </header>
+    )
+}
